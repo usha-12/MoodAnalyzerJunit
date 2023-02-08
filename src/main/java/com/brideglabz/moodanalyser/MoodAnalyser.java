fc3b0614641d318793d5ed717797271cf9af40f0
usha-12 <usha12ahirwar@gmail.com>
2023-02-08 15:27:09 +0530

    Added: Given happy mood return sad mood

--- src/main/java/com/brideglabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/brideglabz/moodanalyser/MoodAnalyser.java
@@ -1,11 +1,15 @@
 package com.brideglabz.moodanalyser;
 
 public class MoodAnalyser {
+    private String message;
+    public void MoodAnalyser(String message) {
+        this.message = message;
+    }
     public String moodAnalyser(String message) {
         if (message.contains("Sad")) {
             return "SAD";
         } else {
-            return "HAPPY";
+            return "SAD";
         }
     }
-}
+            }
