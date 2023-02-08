5ee991385c2b370725c62216c53d135269e33455
usha-12 <usha12ahirwar@gmail.com>
2023-02-08 14:58:07 +0530

    Added: Refactor

--- src/main/java/com/brideglabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/brideglabz/moodanalyser/MoodAnalyser.java
@@ -1,12 +1,14 @@
 package com.brideglabz.moodanalyser;
 
 public class MoodAnalyser {
+    private String message;
+    public void MoodAnalyser(String message) {
+        this.message = message;
+    }
     public String moodAnalyser(String message) {
         if (message.contains("Sad")) {
             return "SAD";
-        }
-        else
-        {
+        } else {
             return "HAPPY";
         }
     }
